647a8b92914aa294d3bdce8a48c42c0bf71374cf
Matt Lian <mattli8312@gmail.com>
2025-04-05 13:50:48 -0500

    Update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development'
+const repo = 'mattli8312.github.io'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -11,10 +14,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	path:{
-		base: '/mattli8312.github.io'
+		adapter: adapter(),
+		path:{
+			base: dev ? '' : `/${repo}`
+		}
 	}
 };
 
